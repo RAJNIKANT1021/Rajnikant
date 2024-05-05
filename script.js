e4d0f73210d753e252ef8fcc35707554999041f5
Rajnikant1021 <rajnikant1021@gmail.com>
2024-05-05 15:53:47 +0530

    undo

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 
 
-const chart = LightweightCharts.createChart(document.getElementById("charts"), { width: 1000, height: 500 });
-const lineSeries = chart.addLineSeries();
-lineSeries.setData([
-    { time: '2019-04-11', value: 80.01 },
-    { time: '2019-04-12', value: 96.63 },
-    { time: '2019-04-13', value: 76.64 },
-    { time: '2019-04-14', value: 81.89 },
-    { time: '2019-04-15', value: 74.43 },
-    { time: '2019-04-16', value: 80.01 },
-    { time: '2019-04-17', value: 96.63 },
-    { time: '2019-04-18', value: 76.64 },
-    { time: '2019-04-19', value: 81.89 },
-    { time: '2019-04-20', value: 74.43 },
-]);
+
 
 var tl=gsap.timeline();
 
